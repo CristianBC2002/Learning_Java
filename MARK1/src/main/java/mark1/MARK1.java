c21a055c7c2a02ef84a5799aa8d7ff9ffa13f8cf
CristianBC2002 <ssjdainidankai@gmail.com>
2024-03-07 17:55:20 +0100

    V1

---
+++ MARK1/src/main/java/mark1/MARK1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mark1;
+
+/**
+ *
+ * @author CRISTIAN
+ */
+public class MARK1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
