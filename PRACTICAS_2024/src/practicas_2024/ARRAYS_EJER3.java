598b8f69e5f095de744c560dc91686c7e77b6856
CristianBC2002 <155979353+CristianBC2002@users.noreply.github.com>
2024-03-07 19:07:34 +0100

    Delete PRACTICAS_2024 directory

--- PRACTICAS_2024/src/practicas_2024/ARRAYS_EJER3.java
+++
@@ -1,9 +0,0 @@
-/*
- boolean estaMatriculadoEn(String asignatura): nos dice si el alumno está matriculado en una asignatura o no.
- */
-package practicas_2024;
-
-
-public class ARRAYS_EJER3 {
-    
-}
