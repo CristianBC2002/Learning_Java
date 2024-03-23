17f9ac30626e6544307ed024130580942054acc0
CristianBC2002 <ssjdainidankai@gmail.com>
2024-03-23 21:36:20 +0100

    Alpha 0.1
    Esta es la versión básica que pide un número del 1 al 100. Pero con un planteamiento diferente y bastante inacabado.

--- RECUPERANDO/src/SentenciasControlDeFlujo/EjWhile2.java
+++ RECUPERANDO/src/SentenciasControlDeFlujo/EjWhile2.java
@@ -1,7 +1,7 @@
 /*
 Programa NúmeroSecreto PARTE 1.
 
-Crear un programa que nos vaya pidiendo números por pantalla del 1 al 100
+-Crear un programa que nos vaya pidiendo números por pantalla del 1 al 100
 hasta que adivinemos el número secreto que serán un valor que
 asignamos previamente.El programa termina si adivinamos el número o si
 pulsamos el 0 para salir.
@@ -13,34 +13,22 @@ import java.util.*;
  class EjWhile2 {
      public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Dime un nº del 1 al 100: ");
         int n = 0;
         int contador = 1;
         int nSecreto = 25;
-              
-      
         
-        while (true){
-            System.out.println("Dime un n del 1 al 100: ");
-            n= sc.nextInt();
+        n= sc.nextInt();
+       
+        while (n!=0 && n!=25){
+            System.out.println("Intentto " +contador++);
+            System.out.println("Numero incorrecto!! Dime otro: ");
+            n = sc.nextInt();
             
-            if (n == 0){
-                System.out.println("Saliste del programa");
+            if (nSecreto<1 && nSecreto<=100){
+                System.out.println("Limite de intentos alcanzado: ");
+                
                 break;
-            }
-            
-            else if (n<1 || n>100){
-                System.out.println("Debes escribir un n entre 1 y 100, vuelve a intentarlo!!");
-                System.out.println("Presiona un caracter pra continurar..");
-                n = sc.nextInt();
-                continue;
-            }
-            
-            else if (n == nSecreto){
-                System.out.println("Bien!! Mision cumplida!!");
-                break;
-            }
-            else {
-                System.out.println("Numero incorrecto!! prueba otra vez!");
             }
             
   
